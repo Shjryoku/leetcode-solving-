0c262d2edd84613dc95b8db0d56bd1f72213ad30
shjryoku@gmail.com <shjryoku@gmail.com>
2026-07-17 18:29:24 +0300

    200

---
+++ 200.number-of-islands.py
@@ -0,0 +1,41 @@
+#
+# @lc app=leetcode id=200 lang=python3
+#
+# [200] Number of Islands
+#
+
+# @lc code=start
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        
+        rows, cols = len(grid), len(grid[0])
+        checked = set()
+
+        islands = 0
+
+        def bfs(r, c):
+            q = collections.deque()
+            checked.add((r,c))
+            q.append((r, c))
+
+            while q:
+                row, col = q.popleft()
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    if (r) in range(rows) and (c) in range(cols) and grid[r][c] == "1" and (r, c) not in checked:
+                        q.append((r, c))
+                        checked.add((r, c))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in checked:
+                    bfs(r, c)
+                    islands += 1
+
+        return islands
+# @lc code=end
+
